3dad48e46a85c4077fa5332bb393ab42c072d4e9
SilasNS <silasschlax@gmail.com>
2026-04-14 14:04:39 -0400

    Add and used rubocop

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,14 +1,12 @@
-
-
-def get_user_text_cipher
+def user_text_cipher
   puts "What text would you like me to add a Caesar Cipher to?"
   gets.chomp
 end
 
-def get_user_shift_index
+def user_shift_index
   puts "What is the shift index you want to use? (Cannot be equal to 0)"
   input = gets.chomp.to_i
-  get_user_shift_index() if input === 0
+  user_shift_index if input.zero?
   input
 end
 
@@ -17,32 +15,29 @@ def shift_alphabet(shift)
   alphabet.rotate(shift)
 end
 
-def shift_text (text, shift)
+def shift_text(text, shift)
   alphabet = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
   shifted_alphabet = shift_alphabet(shift)
-  p shifted_alphabet
-  text = text.split("")
+  text = text.chars
 
-  for i in 0..text.length-1 do
-    if alphabet.include?(text[i].downcase)
-      idx = alphabet.index(text[i].downcase)
-      if text[i] === alphabet[idx]
-        text[i] = shifted_alphabet[idx]
-      else
-        text[i] = shifted_alphabet[idx].upcase
-      end
-    end
+  for i in 0..(text.length - 1) do
+    next unless alphabet.include?(text[i].downcase)
+
+    idx = alphabet.index(text[i].downcase)
+    text[i] = if text[i] == alphabet[idx]
+                shifted_alphabet[idx]
+              else
+                shifted_alphabet[idx].upcase
+              end
   end
-  text.join("")
-end 
-
+  text.join
+end
 
 def run
-  text = get_user_text_cipher
-  shift = get_user_shift_index
+  text = user_text_cipher
+  shift = user_shift_index
   shifted_text = shift_text(text, shift)
   p shifted_text
 end
 
-
-run()
+run
